a078a0cfffa448577a407edfa6e719f39bc4ab2c
--path <goodhopeordu@yahoo.com>
2020-08-04 20:49:09 +0100

    add base colors for application in utils

---
+++ lib/utils.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const white = Colors.white;
+  static const black = Colors.black;
+  static const orangeTag = Color(0xFFEE9405);
+  static const greenIcon = Color(0xFF2EB875);
+  static const greenButton = Color(0xFF34D186);
+  static const greenLight = Color(0xFFD9FDE5);
+  static const greenHighlight = Color(0xFFECFEF2);
+  static const indigo = Color(0xFF6675FF);
+  static const indigoPlaceMark = Color(0xFF6675FE);
+  static const indigoPolyLine = Color(0xFF6776FF);
+  static const indigoVisa = Color(0xFF181E5C);
+  static const indigoDark = Color(0xFF4E5BD0);
+  static const indigoLight = Color(0xFFC4CAFF);
+  static const iconGrey = Color(0xFFBFC0CB);
+  static const textGrey = Color(0xFF82838C);
+  static const textGreySub = Color(0xFF818391);
+  static const shadowGrey = Color(0xFFE3E4E6);
+  static const highlightGrey = Color(0xFFDCDCDC);
+  static const inputGrey = Color(0xFFF4F4F6);
+  static const blackLight = Color(0xFF2F313F);
+  static const blackGreen = Color(0xFF303340);
+}
